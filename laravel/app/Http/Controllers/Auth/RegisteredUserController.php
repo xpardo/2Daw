20973849b02a467a7df37bcb28a1e6ee3302884f
xpardo <xpullodsmx@gmail.com>
2022-10-18 16:09:55 +0200

    b0.1

--- laravel/app/Http/Controllers/Auth/RegisteredUserController.php
+++ laravel/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,6 +48,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+        
 
         return redirect(RouteServiceProvider::HOME);
     }
