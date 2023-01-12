c7914b94d60c807dccc58e2267e113c0aa2882a7
xpardo <xpullodsmx@gmail.com>
2023-01-12 15:37:17 +0100

    test posts

--- laravel/app/Http/Controllers/Api/PostController.php
+++ laravel/app/Http/Controllers/Api/PostController.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Laravel\Sanctum\Sanctum;
-use App\Models\User;
-use App\Models\Like;
+use App\Models\Visibility;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\File;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\api\Posts;
+
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -22,12 +21,10 @@ class PostController extends Controller
      */
     public function index()
     {
- 
-        $posts = Post::all();
         return response()->json([
-            "success"=> true,
-            "data"=>$posts  
-        ]);
+            'success' => true,
+            'data'    => Post::all()
+        ], 200);
     }
 
     /**
@@ -38,52 +35,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        // Validar fitxer
-     
-        $validatedData = $request->validate([
-            'body'          => 'required',
-            'upload'        => 'required',
-            'latitude'      => 'required',
-            'longitude'     => 'required',
-            'visibility_id' => 'required',
-        
-        ]);
-        
-        // Obtenir dades del formulari
-        $body           = $request->get('body');
-        $upload         = $request->file('upload');
-        $latitude       = $request->get('latitude');
-        $longitude      = $request->get('longitude');
-        $visibility_id  = $request->get('visibility_id');
-        
-        // Desar fitxer al disc i inserir dades a BD
-        $file = new File();
-        $fileOk = $file->diskSave($upload);
 
-        if ($fileOk) {
-            // Desar dades a BD
-            \Log::debug("Saving posts at DB...");
-            $posts = Post::create([
-                'body'          => $body,
-                'upload'        => $file->id,
-                'latitude'      => $latitude,
-                'longitude'     => $longitude,
-                'author_id'     => auth()->user()->id,
-                'visibility_id' => $visibility_id,
-            ]);
-            \Log::debug("DB storage OK");
-            // Patró PRG amb missatge d'èxit
+        // Validar fitxer
+        $validatedData = $request->validate([
+            'upload' => 'required|mimes:gif,jpeg,jpg,png|max:2048'
+        ]);
+        // Desar fitxer al disc i inserir dades a BD
+        $upload = $request->file('upload');
+        $file = new File();
+        $ok = $file->diskSave($upload);
+
+        if ($ok) {
             return response()->json([
                 'success' => true,
-                'data'    => $posts
-            ], 201); 
+                'data'    => $file
+            ], 201);
         } else {
-            \Log::debug("Disk storage FAILS");
-            // Patró PRG amb missatge d'error
             return response()->json([
                 'success'  => false,
-                'message' => 'Error storing posts'
-            ],500);
+                'message' => 'Error uploading file'
+            ], 421);
         }
     }
 
@@ -95,23 +66,21 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
-      
-        $posts=Post::find($id);
-        if (empty($posts)) {
-            return response()->json([
-                'success' => false,
-                "message" => "posts ${id} not found"
-            ], 404);
-        }
-        else{
+        $file=File::find($id);
+        if ($file){
             return response()->json([
                 'success' => true,
-                'data'    => $posts
-            ], 201);
-        }
+                'data'    => $file
+            ], 200);
+         }else{
+             return response()->json([
+                 'success' => false,
+                 'message'=> "not found"
+             ], 404);
+         }
+       
     }
-    
+
     /**
      * Update the specified resource in storage.
      *
@@ -122,20 +91,33 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         
-        $posts = Post::find($id);
-        if(empty($posts)){
+        $file=File::find($id);
+        if (empty ($file)) {
             return response()->json([
-                "message" => "posts ${id} not found"
-            ],404);
-        }else{
-            $posts->update($request->all());
-            return \response($posts);
+                'success'  => false,
+                'message' => 'Error not found'
+            ], 404);
         }
-    }
+        // Validar fitxer
+        $validatedData = $request->validate([
+            'upload' => 'required|mimes:gif,jpeg,jpg,png|max:2048'
+        ]);
+        // Desar fitxer al disc i inserir dades a BD
+        $upload = $request->file('upload');
 
-    public function update_workaround(Request $request, $id) // limitació php
-    {
-        return $this->update($request, $id);
+        $ok = $file->diskSave($upload);
+
+        if ($ok) {
+            return response()->json([
+                'success' => true,
+                'data'    => $file
+            ], 200);
+        } else {
+            return response()->json([
+                'success'  => false,
+                'message' => 'Error uploading file'
+            ], 421);
+        }
     }
 
     /**
@@ -146,59 +128,37 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        $posts = Post::find($id);
-        if (empty($posts)) {
-            return \response()->json([
-                "message" => "Post ${id} not found"
+        $file = File::find($id);
+        if (empty ($file)) {
+            return response()->json([
+                'success'  => false,
+                'message' => 'not found'
             ], 404);
-        } else {
-            Post::destroy($id);
-            return \response()->json([
-                "message" => "Post ${id} deleted"
-            ]);
         }
-    }
-    
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     * Likes , Unlike
-     */
-    public function like($id){
-        $user = User::likes($post->author_id);
-        $like = Like::create([
-            'user_id' => $user->id,
-            'post_id' => $post->id,
-        ]);
+        $ok =  $file->diskDelete();
 
-        if($like){
-            return \response()->json([
-                "message" => "Post ${id} not found"
-            ], 401);
-        }
-    }
-    public function unlike($id){
-        $user=User::find($post->author_id);
-        $like = Like::where([
-            ['user_id', "=" ,$user->id],
-            ['post_id', "=" ,$post->id],
-        ]);
-
-        $like->first();
-
-        $like->delete();
-
-        if($like){
-            return \response()->json([
-                "message" => "unlike ${id} not found"
+        if ($ok) {
+            return response()->json([
+                'success' => true,
+                'data'    => $file
             ], 200);
-        }else{
-            Post::destroy($id);
-            return \response()->json([
-                "message" => "Post Unlike ${id} deleted"
-            ]);
+        } else {
+            return response()->json([
+                'success'  => false,
+                'message' => 'Error deleting file'
+            ], 500);
         }
+    }
+    public function update_post(Request $request, $id)
+    {
+        return $this->update($request, $id);
+    }
+    public function delete_post(Request $request, $id)
+    {
+        return $this->destroy($request, $id);
+    }
+    public function show_post(Request $request, $id)
+    {
+        return $this->show($request, $id);
     }
 }
--- laravel/tests/Feature/Api/PostTest.php
+++ laravel/tests/Feature/Api/PostTest.php
@@ -1,51 +1,26 @@
 <?php
 namespace Tests\Feature;
- 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Http\UploadedFile;
-use Tests\TestCase;
+
 use App\Models\User;
-use App\Models\Like;
 use App\Models\Post;
 use App\Models\File;
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Visibility;
+
+
+use Illuminate\Http\UploadedFile;
 
 class PostTest extends TestCase
 {
     public static User $testUser;
     public static array $validData = [];
     public static array $invalidData = [];
-   
-    public static function setUpBeforeClass() : void
-    {
-        parent::setUpBeforeClass();
-        // Creem usuari/a de prova
-        $name = "test_" . time();
-        self::$testUser = new User([
-            "name"      => "{$name}",
-            "email"     => "{$name}@mailinator.com",
-            "password"  => "12345678"
-        ]);
-        // TODO Omplir amb dades vàlides
-        self::$validData = [
-            'body' => 'prova',
-            'upload' => $upload,
-            'latitude'    => '41.2310371',
-            'longitude'   => '1.7282036',
-            'visibility_id'   => 1,
 
-        ];
-        // TODO Omplir amb dades incorrectes
-        self::$invalidData = [
-            'body' => '1',
-            'upload' => '1',
-            'latitude'    => '1',
-            'longitude'   => '1',
-            'visibility_id'   => '1',
-        ];
-    }
+
 
     public function test_post_first()
     {
@@ -56,47 +31,171 @@ class PostTest extends TestCase
             'email' => self::$testUser->email,
         ]);
     }
- 
-    public function test_post_create()
+
+
+
+
+    public function test_post_list()
     {
-        Sanctum::actingAs(self::$testUser);
-        // Cridar servei web de l'API
+        // List all files using API web service
+        $response = $this->getJson("/api/posts");
+        // Check OK response
+        $this->_test_ok($response);
+    }
+
+    public function test_post_create(): object
+    {
+        // Upload fake file using API web service
         $response = $this->postJson("/api/posts", self::$validData);
-        // Revisar que no hi ha errors de validació
-        $params = array_keys(self::$validData);
-        $response->assertValid($params);
-                
         // Check OK response
         $this->_test_ok($response, 201);
-    
+        // Check validation errors
+        $params = array_keys(self::$validData);
+        $response->assertValid($params);
         // Check JSON dynamic values
-        $response->assertJsonPath("post.id",
-            fn ($id) => !empty($id)
+        $response->assertJsonPath(
+            "data.id",
+            fn($id) => !empty($id)
         );
         // Read, update and delete dependency!!!
         $json = $response->getData();
-        return $json->post;
+        return $json->data;
+    }
 
-    }
-  
-    public function test_post_create_error()
+
+
+    public function test_post_update():object
     {
-        Sanctum::actingAs(self::$testUser);
-        // Cridar servei web de l'API
-        $response = $this->postJson("/api/posts", self::$invalidData);
-        // TODO Revisar errors de validació
-        $params = [
-            'body'=> 'body',
-            'upload' => $upload,
-            'latitude'    => 'latitude',
-            'longitude'   => 'longitude',
-            'visibility_id'   => 1,
-        ];
-        $response->assertInvalid($params);
-        
+        // Create fake file
+        $name = "fto.png";
+        $size = 38960; /*KB*/
+        $upload = UploadedFile::fake()->image($name)->size($size);
+        // Upload fake file using API web service
+        $response = $this->putJson("/api/posts/", [
+            "upload" => $upload,
+        ]);
+        // Check OK response
+        $this->_test_ok($response);
+        // Check validation errors
+        $response->assertValid(["upload"]);
+        // Check JSON exact values
+        $response->assertJsonPath("data.filesize", $size * 1024);
+        // Check JSON dynamic values
+        $response->assertJsonPath(
+            "data.filepath",
+            fn($filepath) => str_contains($filepath, $name)
+        );
     }
-  
-    // TODO Sub-tests de totes les operacions CRUD
+    public function test_post_update_error(object $post)
+    {
+        // Create fake file with invalid max size
+        $name = "photo.jpg";
+        $size = 3000; /*KB*/
+        $upload = UploadedFile::fake()->image($name)->size($size);
+        // Upload fake file using API web service
+        $response = $this->putJson("/api/posts/{$post->id}", [
+            "upload" => $upload,
+        ]);
+        // Check ERROR response
+        $this->_test_error($response);
+    }
+    public function test_post_update_notfound()
+    {
+        $id = "not_exists";
+        $response = $this->putJson("/api/posts/{$id}", []);
+        $this->_test_notfound($response);
+    }
+
+    public function test_post_delete(object $post)
+    {
+        // Delete one file using API web service
+        $response = $this->deleteJson("/api/posts/{$post->id}");
+        // Check OK response
+        $this->_test_ok($response);
+    }
+    public function test_post_delete_notfound()
+    {
+        $id = "not_exists";
+        $response = $this->deleteJson("/api/posts/{$id}");
+        $this->_test_notfound($response);
+    }
+
+    protected function _test_ok($response, $status = 200)
+    {
+        // Check JSON response
+        $response->assertStatus($status);
+        // Check JSON properties
+        $response->assertJson([
+            "success" => true,
+            "data" => true // any value
+        ]);
+    }
+    protected function _test_error($response)
+    {
+        // Check response
+        $response->assertStatus(404);
+        // Check validation errors
+        $response->assertInvalid(["upload"]);
+        // Check JSON properties
+        $response->assertJson([
+            "message" => true,
+            // any value
+            "errors" => true, // any value
+        ]);
+        // Check JSON dynamic values
+        $response->assertJsonPath(
+            "message",
+            fn($message) => !empty($message) && is_string($message)
+        );
+        $response->assertJsonPath(
+            "errors",
+            fn($errors) => is_array($errors)
+        );
+    }
+    protected function _test_notfound($response)
+    {
+        // Check JSON response
+        $response->assertStatus(404);
+        // Check JSON properties
+        $response->assertJson([
+            "success" => false,
+            "message" => true // any value
+        ]);
+        // Check JSON dynamic values
+        $response->assertJsonPath(
+            "message",
+            fn($message) => !empty($message) && is_string($message)
+        );
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        // Creem usuari/a de prova
+        $name = "test_" . time();
+        self::$testUser = new User([
+            "name" => "{$name}",
+            "email" => "{$name}@mailinator.com",
+            "password" => "12345678"
+        ]);
+        $name = "avatar.png";
+        $size = 500; /*KB*/
+        $upload = UploadedFile::fake()->image($name)->size($size);
+        // TODO Omplir amb dades vàlides
+        self::$validData = [
+            "body" => "Cos de prova",
+            "upload" => $upload,
+            "latitude" => 41.2,
+            "longitude" => 23.4
+        ];
+        // TODO Omplir amb dades incorrectes
+        self::$invalidData = [
+            "body" => "",
+            "upload" => $upload,
+            "latitude" => "error",
+            "longitude" => "error"
+        ];
+    }
   
     public function test_post_last()
     {
